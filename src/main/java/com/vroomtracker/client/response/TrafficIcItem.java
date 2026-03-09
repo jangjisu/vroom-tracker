5906503a05de459ca400ca9b96df32979cd90353
jangjisu <jjs62611615@gmail.com>
2026-03-09 15:32:24 +0900

    refactor(client): extract ExApiClient inner classes to client/response package
    - TrafficIcResponse, TrafficAllResponse, TrafficFlowResponse
    - TrafficIcItem, TrafficAllItem, TrafficFlowItem
    - 6개 내부 클래스 → client/response/ 독립 파일로 분리

---
+++ src/main/java/com/vroomtracker/client/response/TrafficIcItem.java
@@ -0,0 +1,38 @@
+package com.vroomtracker.client.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/** 톨게이트 입/출구 교통량 항목 */
+@Getter
+@Setter
+@NoArgsConstructor
+public class TrafficIcItem {
+    /** 도공/민자 구분코드 */
+    private String exDivCode;
+    /** 도공/민자 구분명 */
+    private String exDivName;
+    /** 영업소코드 */
+    private String unitCode;
+    /** 영업소명 */
+    private String unitName;
+    /** 입출구 구분코드 (0:입구, 1:출구) */
+    private String inoutType;
+    /** 입출구 구분명 */
+    private String inoutName;
+    /** 자료구분 (1:1시간, 2:15분) */
+    private String tmType;
+    /** 자료구분명 */
+    private String tmName;
+    /** TCS/hi-pass 구분 */
+    private String tcsType;
+    /** TCS/hi-pass 구분명 */
+    private String tcsName;
+    /** 차종구분코드 */
+    private String carType;
+    /** 교통량 (단위: 만대) */
+    private String trafficAmount;
+    /** 집계시간 */
+    private String sumTm;
+}
